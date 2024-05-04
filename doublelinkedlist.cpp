a1f05f53ebdfbe9908fa8654953bde60bc4f0bfc
ahmadjunioor <161430813+ahmadjunioor@users.noreply.github.com>
2024-05-05 01:20:32 +0700

    membuat void deletenode

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -71,3 +71,12 @@ bool search(int rollNo, Node** previous, Node** current)
 	}
 	return (*current != NULL);
 }
+
+void deleteNode()
+{
+	Node* previous, * current;
+	int rollNo;
+
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	cin >> rollNo;
+}
